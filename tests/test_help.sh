ff10dc41e45d0b17b2ff8363a21e03c769581823
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2023-10-02 17:02:33 -0700

    the bin dir is already in PATH .. no need for $TEST_SCRIPTS_DIR

--- tests/test_help.sh
+++ tests/test_help.sh
@@ -6,32 +6,30 @@ set -e
 
 echo "***** test_help.sh *****"
 
-TEST_SCRIPTS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-$TEST_SCRIPTS_DIR/cloneProjectsFromProjectSet.solo -h
-$TEST_SCRIPTS_DIR/createProjectSet.solo -h
-$TEST_SCRIPTS_DIR/createRegistry.solo -h
-$TEST_SCRIPTS_DIR/createStone.solo -h
-$TEST_SCRIPTS_DIR/deleteStone.solo -h
-$TEST_SCRIPTS_DIR/downloadGemStone.solo -h
-$TEST_SCRIPTS_DIR/generateGsDevKitProjectSet.solo -h
-$TEST_SCRIPTS_DIR/gslist.solo -h
-$TEST_SCRIPTS_DIR/loadSmalltalkCIProject.stone -h
-$TEST_SCRIPTS_DIR/loadTode.stone -h
-$TEST_SCRIPTS_DIR/registerProductDirectory.solo -h
-$TEST_SCRIPTS_DIR/registerProduct.solo -h
-$TEST_SCRIPTS_DIR/registerProjectDirectory.solo -h
-$TEST_SCRIPTS_DIR/registerStonesDirectory.solo -h
-$TEST_SCRIPTS_DIR/registerStone.solo -h
-$TEST_SCRIPTS_DIR/registerTodeSharedDir.solo -h
-$TEST_SCRIPTS_DIR/registryReport.solo -h
-$TEST_SCRIPTS_DIR/startNetldi.solo -h
-$TEST_SCRIPTS_DIR/startStone.solo -h
-$TEST_SCRIPTS_DIR/stopNetldi.solo -h
-$TEST_SCRIPTS_DIR/stopStone.solo -h
-$TEST_SCRIPTS_DIR/template.solo -h
-$TEST_SCRIPTS_DIR/todeIt.solo -h
-$TEST_SCRIPTS_DIR/updateCustomEnv.solo -h
-$TEST_SCRIPTS_DIR/updateProjectSet.solo -h
-$TEST_SCRIPTS_DIR/versionReport.gs_solo -h
-$TEST_SCRIPTS_DIR/versionReport.solo -h
+cloneProjectsFromProjectSet.solo -h
+createProjectSet.solo -h
+createRegistry.solo -h
+createStone.solo -h
+deleteStone.solo -h
+downloadGemStone.solo -h
+generateGsDevKitProjectSet.solo -h
+gslist.solo --help
+loadSmalltalkCIProject.stone -h
+loadTode.stone -h
+registerProductDirectory.solo -h
+registerProduct.solo -h
+registerProjectDirectory.solo -h
+registerStonesDirectory.solo -h
+registerStone.solo -h
+registerTodeSharedDir.solo -h
+registryReport.solo --help
+startNetldi.solo -h
+startStone.solo -h
+stopNetldi.solo -h
+stopStone.solo --help
+template.solo -h
+todeIt.solo -h
+updateCustomEnv.solo -h
+updateProjectSet.solo --help
+versionReport.gs_solo -h
+versionReport.solo -h
