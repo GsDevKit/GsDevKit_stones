5da7605939e790d91acbdbbeb3fdf14f9cd9174c
Johan Brichau <johan@yesplan.be>
2023-10-22 16:25:20 +0200

    branch correct superDoit branch in install.sh script and install correct PATHs

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,16 @@
 #! /usr/bin/env bash
 
-git clone git@github.com:dalehenrich/superDoit.git &&
+SUPERDOIT_BRANCH=v4.1
+
+git clone --branch $SUPERDOIT_BRANCH git@github.com:dalehenrich/superDoit.git &&
 cd superDoit/bin &&
 ./install.sh
+
+export PATH=`pwd`/superDoit/bin:`pwd`/GsDevKit_stones/bin:$PATH
+versionReport.solo
+export GSDEVKIT_STONES_ROOT=`pwd`/GsDevKit_stones
+export STONES_DATA_HOME=$XDG_DATA_HOME
+if [ "$STONES_DATA_HOME" = "" ] ; then
+	# on Mac ensure the directory you choose exists
+	export STONES_DATA_HOME=$HOME/.local/share
+fi
