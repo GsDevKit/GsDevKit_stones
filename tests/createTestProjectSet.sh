18ff18687cfb61cf5cfc9c15a7141753f84f4544
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2023-10-23 10:04:51 -0700

    fix typo in createTestProjectSet.sh ... improve ci.yml names

--- tests/createTestProjectSet.sh
+++ tests/createTestProjectSet.sh
@@ -15,7 +15,7 @@ if [ ! -d $STONES_HOME/test_git ]; then
 	mkdir $STONES_HOME/test_git
 fi
 
-registryReport.solo --registry=$registry --ensure
+createRegistry.solo --registry=$registry --ensure
 
 set +e
 createProjectSet.solo --registry=$registry --projectSet=$projectSet --empty $*
