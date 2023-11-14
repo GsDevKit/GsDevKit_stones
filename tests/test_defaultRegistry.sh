8f261ed49615c2230c8ca6b1621bf38142220ffd
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2023-11-14 13:16:02 -0800

    add back test_defaultRegistry.sh logging

--- tests/test_defaultRegistry.sh
+++ tests/test_defaultRegistry.sh
@@ -141,6 +141,10 @@ pushd $STONES_HOME/test_stones/stones/$stoneName
 	if [ ! -d "snapshots" ]; then
 		mkdir snapshots
 	fi
+	ls -altr
+	cat .topazini
+	cat .GDKStoneSpec.ston
+	versionReport.stone
 	snapshot.stone --extension=`date +%m-%d-%Y_%H:%M:%S`.dbf snapshots --safely $*
 	# should remove the requirement for -r ...
 	newExtent.solo -r $defaultRegistryName -e snapshots/*.dbf $stoneName $*
